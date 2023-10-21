a63189a92047f5cc96823aa2c770116fa7360eb2
Ivan Sholana <ivansholana@gmail.com>
2023-10-21 07:27:00 +0700

    add enigma

--- enigma.py
+++ enigma.py
@@ -1,6 +1,6 @@
-rotor_1 = "RBJTMVAQCXHKSYNGPLWIFDOUEZ"
-rotor_2 = "LUCQGWVRFDHJSZBXPKNIYOAEMT"
-rotor_3 = "TABDMKWJOIPXNLVFCQZGEYSRUH"
+rotor_1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ" # Rotor_ 1 mapping
+rotor_2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE" # Rotor _2 mapping
+rotor_3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO" # Rotor 3_ mapping
 
 alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -12,23 +12,23 @@ def rotor_encription(character):
     global rotor_1, rotor_2, rotor_3, rotor_1Position, rotor_2Position, rotor_3Position
    
     rotor1 = rotor_1[alfabet.index(character)]
-    rotor_1 = rotor_1[-1] + rotor_1[0:len(rotor_1)-2]
+    rotor_1 = rotor_1[-1] + rotor_1[0:-1]
     rotor_1Position += 1
 
     rotor2 = rotor_2[alfabet.index(rotor1)]
     if rotor_1Position > len(alfabet):
-        rotor_2 = rotor_2[-1] + rotor_2[0:len(rotor_2)-2]
+        rotor_2 = rotor_2[-1] + rotor_2[0:-1]
         rotor_2Position += 1
         rotor_1Position = 0
 
     rotor3 = rotor_3[alfabet.index(rotor2)]
     if rotor_2Position > len(alfabet):
-        rotor_3 = rotor_3[-1] + rotor_3[0:len(rotor_3)-2]
+        rotor_3 = rotor_3[-1] + rotor_3[0:-1]
         rotor_2Position = 0
     
     return rotor3
 
-def switch_pludge(character):
+def plugboard(character):
     pludge_board = [["A", "Y"], ["D", "X"]]
     for i in pludge_board:
         if character in i:
@@ -36,10 +36,10 @@ def switch_pludge(character):
         return character
      
 def encription_process(character):
-    character = switch_pludge(character)
+    character = plugboard(character)
     character = rotor_encription(character)
     character = rotor_encription(character)
-    character = switch_pludge(character)
+    character = plugboard(character)
     return character
 
 pesan = input("masukkan pesan : ").upper()
